3b73a916906126706e5328a6a87b1b7c2047fda3
arha83 <amirrhosseinali@gmail.com>
2024-06-05 22:50:24 +0330

    modified first view

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String[] args) {
 
 
-        new PurchaseController(Customer.loadCustomers().get(1), new BookStore(Book.loadBooks()));
+        new LoginController();
 
     }
 }
